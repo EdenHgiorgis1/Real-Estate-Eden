7a2cb6ae5fc9cf7e26acd676df858a15166ba207
EdenHGiorgis <edenhgiorgis21@gmail.com>
2022-10-28 21:56:41 +0300

    popups

---
+++ RealEstateEden/RealEstateEden/Pop Ups/EstatePopUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealEstateEden.Pop_Ups
+{
+    public partial class EstatePopUp : Form
+    {
+        public EstatePopUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
